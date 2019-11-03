38d71f3b6166edca2214d2bf35d93b3043388e8d
TianleJin <57119885+TianleJin@users.noreply.github.com>
2019-11-03 14:10:34 +1100

    Update Gomoku.py

--- Gomoku.py
+++ Gomoku.py
@@ -2,7 +2,7 @@ from random import randrange
 from cachetools import LRUCache
 from timeit import default_timer
 
-# comment
+
 class ConnectFiveBoard:
 
     """This class offers a template for the game board used in the game of Gomoku a.k.a Connect 5.
